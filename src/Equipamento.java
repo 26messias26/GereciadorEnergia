29a9d791962f6396a3fda58e9334c56cd7549169
26messias26 <messias26severino@gmail.com>
2023-07-02 17:15:57 -0300

    Classe Equipamento

--- src/Equipamento.java
+++ src/Equipamento.java
@@ -1,3 +1,16 @@
 public class Equipamento {
 
+    private EstadoEquipamento estado;
+
+    public Equipamento(){
+        this.estado = new EstadoDesligado(this);
+    }
+    public void desligar(){
+        System.out.println("[GE] Desligando equipamentos...");
+
+    }
+
+    public EstadoEquipamento getEstado() {
+        return estado;
+    }
 }
